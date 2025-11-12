cc407da3a50beb569070a15df53671a5420f3525
sazalahmed <sazal.web@gmail.com>
2025-11-12 23:10:12 +0600

    figma to html done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 $(function () {
 
     "use strict";
+
+
     // svg icon converter
     SVGInject(document.querySelectorAll("img.injectable"));
+
 
     // progressbar
     $(document).ready(function () {
@@ -11,10 +14,49 @@ $(function () {
         });
     });
 
+
     // venobox
     $('.venobox').venobox();
 
 
+    // slick slider
+    $('.team_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="fa-solid fa-chevron-right nextArrow"></i>',
+        prevArrow: '<i class="fa-solid fa-chevron-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 
 
 
